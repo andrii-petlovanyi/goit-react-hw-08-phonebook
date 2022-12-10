b9288d43a82c3b24dee0da47bdb7bce3b10f5683
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-12-11 01:11:41 +0200

    added burger menu

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,14 +1,4 @@
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Container,
-  Flex,
-  HStack,
-  IconButton,
-  useBreakpointValue,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { IconButton, useColorModeValue } from '@chakra-ui/react';
 import * as React from 'react';
 import { FiMenu } from 'react-icons/fi';
 
