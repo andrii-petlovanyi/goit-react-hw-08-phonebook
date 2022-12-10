0a6d44f71db9641179a211ecae9df199890189ac
andrii-petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-12-10 22:15:13 +0000

    Deploying to gh-pages from @ andrii-petlovanyi/goit-react-hw-08-phonebook@9e03052302b55ead4b2dda6bde0616ff71244632 🚀

--- static/js/200.cc109255.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[200],{6643:function(e,h,n){n.r(h);var s=n(7689),t=n(9712),a=n(4295),x=n(2791),i=n(3329);h.default=function(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(a.xG,{}),(0,i.jsx)(t.xu,{as:"main",maxWidth:"1200px",height:"80vh",width:"100%",mx:"auto",px:"15px",children:(0,i.jsx)(x.Suspense,{fallback:!1,children:(0,i.jsx)(s.j3,{})})}),(0,i.jsx)(a.$_,{})]})}}}]);
-//# sourceMappingURL=200.cc109255.chunk.js.map
