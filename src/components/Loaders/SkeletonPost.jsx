6cef112a44cb279a9d15d334662bbcd567ee1977
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-12-10 22:41:40 +0200

    optimised skeleton post item

--- src/components/Loaders/SkeletonPost.jsx
+++ src/components/Loaders/SkeletonPost.jsx
@@ -9,12 +9,13 @@ export const SkeletonPost = () => {
   return (
     <>
       <Box
-        padding="3"
+        padding="4"
         boxShadow="lg"
+        borderRadius="10px"
         bg={useColorModeValue('white', '#2D3748')}
       >
         <SkeletonCircle size="6" />
-        <SkeletonText mt="4" noOfLines={2} spacing="4" skeletonHeight="2" />
+        <SkeletonText mt="4" noOfLines={3} spacing="4" skeletonHeight="2" />
       </Box>
     </>
   );
