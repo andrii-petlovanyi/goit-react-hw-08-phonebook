34e443a8321ba37ad6aa8f1b202685ca4005cea2
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-12-10 22:52:24 +0200

    delete no-unsed vars

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import {
-  ChakraProvider,
-  ColorModeProvider,
-  ColorModeScript,
-  CSSReset,
-} from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { store, persistor } from './redux/store';
 import { App } from './App';
 import './index.css';
