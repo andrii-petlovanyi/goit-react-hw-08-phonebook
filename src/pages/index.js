42a9045d9deb4916b794516cb9dd4813fdcf41ae
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-12-16 21:03:39 +0200

    comment reactStrictMode

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import { lazy } from 'react';
 
 export const Layout = lazy(() => import('pages/Layout'));
 export const Home = lazy(() => import('pages/Home'));
-export const Login = lazy(() => import('pages/Login'));
-export const Register = lazy(() => import('pages/Register'));
 export const Contacts = lazy(() => import('pages/Contacts'));
 export const AddContacts = lazy(() => import('pages/AddContacts'));
+export const Login = lazy(() => import('pages/Login'));
+export const Register = lazy(() => import('pages/Register'));
 export const NotFound = lazy(() => import('pages/404'));
